c9fd3938e49d1bc00141bcf6947377195ffcb578
WangTao <1609832760@qq.com>
2023-05-04 20:44:35 +0800

    C语言学习2023新

---
+++ c/day_5_4/day_5_4/test.c
@@ -0,0 +1,121 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int DigitSum(int n)
+//{
+//	int sum = 0;
+//	if (n > 0)
+//	{
+//		sum = n % 10 + DigitSum(n / 10);
+//	}
+//	return sum;
+//}
+//
+//int main()
+//{
+//	int ret = DigitSum(172);
+//	printf("%d\n", ret);
+//
+//	return 0;
+//}
+
+//int MyStrlen1(char* ch)
+//{
+//	int len = 0;
+//	while (*ch != '\0')
+//	{
+//		len++;
+//		ch++;
+//	}
+//	return len;
+//}
+//int len = 0;
+//int MyStrlen2(char* ch)
+//{	
+//	while (*ch != '\0')
+//	{
+//		len = 1 + MyStrlen2(ch + 1);
+//		break;
+//	}
+//
+//	return len;
+//}
+//
+//int main()
+//{
+//	char ch[] = "hello";
+//	
+//	int ret1 = MyStrlen1(ch);
+//	int ret2 = MyStrlen2(ch);
+//
+//	printf("%d\n", ret1);
+//	printf("%d\n", ret2);
+//
+//	return 0;
+//}
+
+
+//int func1(int n)
+//{
+//	if (n == 0)
+//		return 1;
+//	
+//
+//	int ret = 1;
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret*= i;
+//	}
+//	return ret;
+//}
+//
+//int func2(int n)
+//{
+//	if (n == 0)
+//		return 1;
+//
+//	int ret = 1;
+//	while (n > 0)
+//	{
+//		ret = n * func2(n - 1);
+//		break;
+//	}
+//	return  ret;
+//}
+//
+//int main()
+//{
+//	int n = 4;
+//
+//	int ret1 = func1(n);
+//	int ret2 = func2(n);
+//
+//	printf("%d\n", ret1);
+//	printf("%d\n", ret2);
+//
+//	return 0;
+//}
+
+
+void print(int n)
+{	
+	int ret = 0;
+	while (n > 0)
+	{
+		ret = n % 10;
+		print(n / 10);
+		printf("%d ", ret);
+		break;
+	}
+
+}
+
+int main()
+{
+	int a = 1205;
+	print(a);
+
+	return 0;
+}
