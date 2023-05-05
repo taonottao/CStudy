c6f6019a5b67185efcf59d3af965e67a5178a288
WangTao <1609832760@qq.com>
2023-05-05 22:56:15 +0800

    C语言2023学习   新

---
+++ c/day_5_5/day_5_5/test.c
@@ -0,0 +1,126 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//    char str[] = "hello bit";
+//    printf("%d %d\n", sizeof(str), strlen(str));
+//    return 0;
+//}
+
+
+
+//void swap(int* arr1, int* arr2, int sz)
+//{
+//	int tmp = 0;
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		tmp = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = tmp;
+//	}
+//}
+//
+//int main()
+//{
+//	int arr1[5] = { 1,2,3,4,5 };
+//	int arr2[5] = { 6,7,8,9,10 };
+//	int sz = sizeof(arr1) / sizeof(arr1[0]);
+//	int i = 0;
+//	swap(arr1, arr2, sz);
+//
+//	for (i = 0; i < sz; i++) {
+//		printf("%d ", arr1[i]);
+//	}
+//	printf("\n");
+//	for (i = 0; i < sz; i++) {
+//		printf("%d ", arr2[i]);
+//	}
+//
+//	return 0;
+//}
+
+//void init(int* arr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = 0;
+//	}
+//}
+//
+//void print(int* arr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+//
+//void reverse(int* arr, int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	int tmp = 0;
+//	while (left < right)
+//	{
+//		tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[5] = { 1,2,3,4,5 };
+//	print(arr, 5);
+//	reverse(arr, 5);
+//	printf("\n");
+//	print(arr, 5);
+//	init(arr, 5);
+//	printf("\n");
+//	print(arr, 5);
+//
+//	return 0;
+//}
+
+void bubbleSort(int* arr, int sz)
+{
+	int i = 0;
+	int tmp = 0;
+	int flag = 1;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++) {
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+		{
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int arr[5] = { 5,4,3,2,1 };
+	bubbleSort(arr, 5);
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
